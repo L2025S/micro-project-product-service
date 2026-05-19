1fc739c2f62bc03f63197f664f6065a123212b12
L.W. <reaktor.utfallen-2z@icloud.com>
2026-05-19 15:08:48 +0200

    Initial commit

---
+++ src/main/java/se/iths/lw/microprojectproductservice/MicroProjectProductServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.lw.microprojectproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProjectProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroProjectProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/lw/microprojectproductservice/MicroProjectProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.lw.microprojectproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroProjectProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
