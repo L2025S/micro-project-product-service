5b45d984d19d1861be363aefcdc3abce697baa7a
L.W. <reaktor.utfallen-2z@icloud.com>
2026-05-21 14:32:43 +0200

    Update the auth-server-url

---
+++ src/test/java/se/iths/lw/microprojectproductservice/service/ProductServiceTest.java
@@ -0,0 +1,52 @@
+package se.iths.lw.microprojectproductservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByUuid() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void reduceStock() {
+    }
+
+    @Test
+    void increaseStock() {
+    }
+
+    @Test
+    void updateBasicInfo() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteByUuid() {
+    }
+
+    @Test
+    void decreaseStockBatch() {
+    }
+}
