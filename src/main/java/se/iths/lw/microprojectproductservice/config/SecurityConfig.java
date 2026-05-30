e5cf443c779c40da2703efc1729f64e8e450f5c8
L.W. <reaktor.utfallen-2z@icloud.com>
2026-05-30 17:12:59 +0200

    Add jwtDecoder to SecurityFilterChain

--- src/main/java/se/iths/lw/microprojectproductservice/config/SecurityConfig.java
+++ src/main/java/se/iths/lw/microprojectproductservice/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity httpSecurity,
+            JwtDecoder jwtDecoder,
             JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
@@ -75,7 +76,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt ->
-                        jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+                        jwt.decoder(jwtDecoder).jwtAuthenticationConverter(jwtAuthenticationConverter)));
         httpSecurity.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
 
         return httpSecurity.build();
